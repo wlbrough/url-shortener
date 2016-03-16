20378a0990eac8e7134c840c58d50e2cda8ae4bd
Bill Broughton <wlbrough@gmail.com>
2016-03-16 17:23:56 +0000

    Need to ensure http prefix on links

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 var http = require('http');
 var path = require('path');
 var express = require('express');
+var mongo = require('mongodb').MongoClient;
+var Hashids = require('hashids');
+var hashids = new Hashids();
 
 var router = express();
 var server = http.createServer(router);
+var db = require('monk')('localhost/local');
+
+var links = db.get('links');
+
+router.get("/", function(req, res) {
+  // do something
+});
+
+router.get("/new/:full_url", function(req, res) {
+  var host = req.get('Host');
+  links.insert({ 'fullLink': req.params.full_url }, function(err, doc) {
+    if (err) throw err;
+    res.json({ 'original_url': req.params.full_url, 'short_url': host + '/' + hashids.encodeHex(doc._id) });
+  });
+});
+
+router.get("/:stub", function(req, res) {
+  var objectId = hashids.decodeHex(req.params.stub);
+  links.findById(objectId, function(err, doc) {
+    if (err) throw err;
+    var link = doc.fullLink;
+    res.redirect(link);
+  });
+});
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
